025d6fe6f31f3f82f6a2a155fb587c4d04d830e2
Naoto3615 <supra3615@gmail.com>
2020-04-28 13:32:04 +0900

    chatspace
    最新版

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-    process resize_to_fit: [50, 50]
+    process resize_to_fit: [200, 200]
   # def scale(width, height)
   #   # do something
   # end
